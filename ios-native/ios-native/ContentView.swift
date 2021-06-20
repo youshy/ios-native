bc05c02e0f189a3abe9e7468aa4b0814e33936ec
arturkondas <artur.kondas@ecs-digital.co.uk>
2021-06-20 22:20:18 +0100

    initial commit

---
+++ ios-native/ios-native/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-native
+//
+//  Created by Artur Kondas on 20/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I am the danger!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-native/ios-native/ios_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_nativeApp.swift
+//  ios-native
+//
+//  Created by Artur Kondas on 20/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ios_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
